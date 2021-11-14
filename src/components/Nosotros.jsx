1a8180b2a878ad0916edb9f051a736ba1fd401c1
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-11-13 21:02:37 -0500

    [ UPDATE ] State of the app when the app renders

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Nosotros = () => {
+    useEffect(() => {
+        console.log('USEEFFECT')
+        setEquipo(datos)
+    },[])
+
+    const datos = [
+        {
+            id: 1,
+            nombre: 'React',
+        },
+        {
+            id: 2,
+            nombre: 'Vuejs',
+        },
+        {
+            id: 3,
+            nombre: 'Angular',
+        }
+    ]
+
+    const [equipo, setEquipo] = useState(null)
     return (
         <div>
             <h1>Nosotros</h1>
