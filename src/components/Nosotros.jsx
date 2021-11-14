aa5975a249dff1deb05ef4cc33c4355537ad37c0
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-11-13 21:17:02 -0500

    [ UPDATE ] Call to API using useEffect and saving the API's result in the local state to display it in the view

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -2,29 +2,27 @@ import React, {useEffect, useState} from 'react'
 
 const Nosotros = () => {
     useEffect(() => {
-        console.log('USEEFFECT')
-        setEquipo(datos)
+        obtenerDatos()
+        
     },[])
 
-    const datos = [
-        {
-            id: 1,
-            nombre: 'React',
-        },
-        {
-            id: 2,
-            nombre: 'Vuejs',
-        },
-        {
-            id: 3,
-            nombre: 'Angular',
-        }
-    ]
+    const obtenerDatos = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await data.json()
+        setEquipo(users)
+    }
+    const [equipo, setEquipo] = useState([])
 
-    const [equipo, setEquipo] = useState(null)
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map(user => (
+                        <li key={user.id}>{user.name} - {user.email}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
